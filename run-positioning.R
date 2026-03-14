41f3c832b0cb51dd1e29887e30a8633b8d2d0188
mhl34 <67287623+mhl34@users.noreply.github.com>
2026-03-14 12:21:40 -0400

    Remove unnecessary setwd call in run-positioning.R

--- run-positioning.R
+++ run-positioning.R
@@ -6,8 +6,6 @@
 ### Input: path to SBcounts.h5 (output of spatial-count.jl)
 ### Output: obj.qs, summary.pdf (in addition to positioning.R outputs)
 ################################################################################
-
-setwd('/broad/macosko/leematth/projects/PD/Macosko-Pipelines/slide-tags')
 stopifnot(file.exists("positioning.R", "helpers.R", "plots.R"))
 suppressMessages(source("helpers.R"))
 suppressMessages(source("plots.R"))
